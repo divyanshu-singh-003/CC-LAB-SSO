ef2207cf2f930656d19e44a8d5700abbc7f7bf2c
Divyanshu Singh <divyanshusingh12082003@gmail.com>
2024-04-12 16:47:40 +0530

    third commit

--- frontend/src/pages/page1/Page1.jsx
+++ frontend/src/pages/page1/Page1.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import useLogout from '../../../hooks/useLogout';
 import { useAuthContext } from '../../context/AuthContext';
+
+import {Link} from "react-router-dom"
 const Page1 = () => {
 
     const { loading, logout } = useLogout();
@@ -14,9 +16,16 @@ const Page1 = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl mb-8">Welcome to Page 1, {authUser.fullName}</h1>
+      <div className="flex justify-center gap-5">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>
         Logout
       </button>
+
+      <Link to ={"/google"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        Page 2
+      </Link>
+      </div>
+
     </div>
   );
 };
--- frontend/src/pages/page1/Page2.jsx
+++ frontend/src/pages/page1/Page2.jsx
@@ -4,6 +4,8 @@ import useLogout from '../../../hooks/useLogout';
 
 import { useAuthContext } from '../../context/AuthContext';
 
+import { Link } from 'react-router-dom';
+
 const Page2 = () => {
     const {authUser} = useAuthContext();
     const { loading, logout } = useLogout();
@@ -15,9 +17,14 @@ const Page2 = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl mb-8">Welcome to Page 2, {authUser.fullName}</h1>
+      <div className="flex gap-5">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>
         Logout
       </button>
+      <Link to ={"/"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+        Page 1
+      </Link>
+      </div>
     </div>
   );
 };
